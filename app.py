4f90ca3fc9a013804e026710ee65fb7ada0a153e
Joel Llacsahuache Copia <joel.llacsahuache@upeu.edu.pe>
2022-10-02 20:05:51 -0500

    Creando las rutas

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 
 
 app= Flask(__name__)
 
+app.config['MYSQL_HOST']='us-cdbr-east-06.cleardb.net'
+app.config['MYSQL_USER']='b8e78062f3909c'
+app.config['MYSQL_PASSWORD']='64532001'
+app.config['MYSQL_BD']='heroku_631eb2166968388'
+mysql= MySQL(app)
+CORS(app)
+
 @app.route('/')
-def inciarApp():
-    return 'Inicio'
+def iniciarApp():
+    return render_template('inicio.html')
 
 @app.route('/empleado')
 def mostrarEmpleados():
-    return 'Empleados'
+    return render_template('empleados/empleado.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
